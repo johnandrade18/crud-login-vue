3f165ea03a4187cf89a3330d4dc397e91089c00e
JohnAndrade <johnandrade18@hotmail.com>
2021-03-16 17:19:14 -0500

    updated servicios.js

--- src/servicios.js
+++ src/servicios.js
@@ -1,16 +1,10 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-  },
-});
-
 export default class Servicios {
   url = "https://crud-api-nodejs.herokuapp.com";
 
   getAll() {
-    return axiosInstance.get(this.url, {
+    return axios.get(this.url, {
       method: "GET",
       mode: "no-cors",
       headers: {
