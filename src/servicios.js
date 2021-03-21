096f0abfa838f55aff684070566ef17b10424c7b
JohnAndrade <johnandrade18@hotmail.com>
2021-03-20 19:00:59 -0500

    updated servicios.js

--- src/servicios.js
+++ src/servicios.js
@@ -4,14 +4,6 @@ export default class Servicios {
   url = "https://crud-api-nodejs.herokuapp.com";
 
   getAll() {
-    return axios.get(this.url, {
-      method: "GET",
-      mode: "no-cors",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Methods": "GET",
-        "Access-Control-Allow-Origin": "*",
-      },
-    });
+    return axios.get(this.url);
   }
 }
